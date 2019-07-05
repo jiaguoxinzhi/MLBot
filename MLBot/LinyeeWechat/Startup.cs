a52f06b87592a400e2ea88b74c2662e740dc2808
jiaguoxinzhi <jiaguoxinzhi@qq.com>
2019-07-05 09:39:51 +0800

    初定.net core版本

--- MLBot/LinyeeWechat/Startup.cs
+++ MLBot/LinyeeWechat/Startup.cs
@@ -4,17 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace MLBot.Wechat
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,31 +29,77 @@ namespace MLBot.Wechat
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                // options.CheckConsentNeeded = context => true;
+                //options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(optsions =>
+                {
+                    optsions.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+                });
+
+            // 设置 Session 过期时间
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(1);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IHostingEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Error");
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseMiddleware(typeof(ErrorHandlingMiddleware));
 
-            app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseWebSockets();
+            //app.UseMiddleware(typeof(WebSocketMiddleware));
 
-            app.UseEndpoints(endpoints =>
+            app.UseSession();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseStaticFiles(new StaticFileOptions
             {
-                endpoints.MapControllers();
+                //FileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory()),
+                //设置不限制content-type 该设置可以下载所有类型的文件，但是不建议这么设置，因为不安全
+                //ServeUnknownFileTypes = true
+                //下面设置可以下载apk和nupkg类型的文件
+                ContentTypeProvider = new FileExtensionContentTypeProvider(
+                   new Dictionary<string, string>
+                   {
+                        { ".apk","application/vnd.android.package-archive"},
+                        { ".ipa","application/octet-stream.ipa"},
+                        { ".nupkg","application/zip"}
+
+                   })
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                  name: "areas",
+                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
