6458b28e8479e54293ecd20a04c1a2f894623990
jiaguoxinzhi <jiaguoxinzhi@qq.com>
2019-08-07 18:01:39 +0800

    NotNull

---
+++ MLBot/MLBot_Common/Attribute/NotNullAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLBot
+{
+    /// <summary>
+    /// 不要为Null
+    /// </summary>
+    /// Linyee 2019-08-05
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class NotNullAttribute : Attribute
+    {
+        public NotNullAttribute()
+        {
+
+        }
+
+        public override bool Match(object obj)
+        {
+            if (obj == null) return false;
+            return true;
+        }
+    }
+}
